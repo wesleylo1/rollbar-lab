a06b895a1b0ceb3266e16618501717c4d62dfa3a
Wesley Lo <wesleylo1995@gmail.com>
2022-02-10 14:12:02 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+
+app.get(express.json())
+app.get(cors())
+app.use(express.static('public'))
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+
+const port = process.env.PORT || process.env.SERVER_PORT
+
+app.listen(port, ()=> console.log(`Server is running on port ${port}`))
