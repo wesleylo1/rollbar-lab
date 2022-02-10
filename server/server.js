ac7c572cc138459d09e8f3bc7176538d99a4d87e
Wesley Lo <wesleylo1995@gmail.com>
2022-02-10 15:30:57 -0600

    path update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+let { SERVER_PORT,ROLLBAR_TOKEN } = process.env
+
 const express = require('express')
 const cors = require('cors')
 
@@ -7,16 +9,24 @@ const app = express()
 
 const path = require('path')
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 
 app.get(express.json())
 app.get(cors())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../client/'))) // follow up with this
 
 app.get('/',(req,res) =>{
+    rollbar.log('accessed')
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 
-const port = process.env.PORT || process.env.SERVER_PORT
+const port = process.env.PORT || SERVER_PORT
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
